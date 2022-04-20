8ab071c6bee7aef548356035c7eeac626a3053c6
DJrMartin <tinmar.dm@gmail.com>
2022-04-20 15:50:00 +0200

    upload data

---
+++ Data_manipulation/Pretraitement_data.R
@@ -0,0 +1,24 @@
+###Import
+User='David'
+if (User=='David'){
+  load('C:/Users/David/Desktop/MICROBIOTA_SPATIAL_FLIGHT/DATA_MICROGRAVITY_PROJECT.RData')
+}
+if (User=='Marion'){
+  load()
+}
+if (User=='Valerie'){
+  load()
+}
+rm(User)
+###Courbe de rarefaction
+package(??)
+
+###Normalisation
+install.packages("metagenomeSeq")
+
+
+###Diversity
+#richesse, alpha et beta
+
+
+###log poisson.
