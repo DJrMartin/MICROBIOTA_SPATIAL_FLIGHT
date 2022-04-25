250086f5dca427b79cfb0abd015a38c91d082311
DJrMartin <tinmar.dm@gmail.com>
2022-04-25 15:44:41 +0200

    updata

--- Data_manipulation/PLN_Models.R
+++ Data_manipulation/PLN_Models.R
@@ -2,11 +2,27 @@
 #https://oliviergimenez.github.io/code_livre_variables_cachees/chiquet.html
 
 library('PLNmodels')
+library(corrplot)
+library(dplyr)
 
-data(trichoptera)
-trichoptera <- prepare_data(trichoptera$Abundance, trichoptera$Covariate)
+###Preparation data
+experimental_condition$Conditions=as.factor(substr(experimental_condition$Sample, 9,12))
+PLN_data<-prepare_data(counts = matrix_otu, covariates = experimental_condition[, c(1,2,4)])
 
-myPLN <- PLN(Abundance ~ 1, data = trichoptera)
-coef(myPLN)
-myPCA <- PLNPCA(Abundance ~ 1, data = trichoptera, ranks = 1:8)
+###Model de base
 
+PLN_model=PLN(Abundance~ 0 + time + Conditions, data=PLN_data)
+
+Coefficient<-PLN_model %>% 
+  coefficients()
+filtre=Coefficient[,2] %>% order(decreasing = T)
+
+Coefficient[filtre[1:10],2]%>% t() %>% round(1) %>% 
+  corrplot(is.corr = FALSE, method = 'color', tl.cex = .5, cl.pos = "n")
+
+plot(matrix_otu[,filtre[2]]~experimental_condition$time)
+species[filtre[2],]
+
+###Reduction de dimensionalite
+PLN_PCA <- PLNPCA(Abundance~ 0 + time + Conditions, 
+                  data=PLN_data,ranks = 1:20)
--- Data_manipulation/Pretraitement_metalome.R
+++ Data_manipulation/Pretraitement_metalome.R
@@ -1,2 +1,3 @@
 ##Pretraitement du metalome et gestion du fichier pour le merger aux donnees microbiote.
 
+
