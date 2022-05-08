6035e56313554d8e0ff3e460f1e8cdb0eae4f83f
David_MacOS <david.martin@univ-rennes2.fr>
2022-05-08 17:36:29 +0200

    test avec subsampling de 200 cluster pour étudier la variabilite des indices de diversity

--- Data_manipulation/deep_sequencing_biais.R
+++ Data_manipulation/deep_sequencing_biais.R
@@ -3,13 +3,13 @@ library(fossil)
 ###Import#####################################################################
 User='David'
 if (User=='David'){
-  load("/Users/martind/Dropbox/EXOMIC 2022/DATA_PROJECT_1.RData")
+  load( "/Users/martind/Desktop/RData_Microgravity_updata.RData")
 }
 if (User=='Marion'){
   load()
 }
 rm(User)
-layout(matrix(c(1,2,3), nrow=3))
+
 ind=matrix_otu[2,which(matrix_otu[2,]>0)] #modifier la borne, l'individu..
 rare_curve=NULL
 
@@ -18,6 +18,7 @@ for (i in 1:length(ind)){
   rare_curve=c(rare_curve,new)
 }
 
+##Expression des indices de diversite en fonction de la profondeur de séquençage.
 rc=rshannon=rchao=NULL
 for (j in seq(1,length(rare_curve), by=100)){
   u=sample(as.factor(rare_curve), size=j)
@@ -25,8 +26,6 @@ for (j in seq(1,length(rare_curve), by=100)){
   rshannon=c(rshannon,diversity(table(u), index='simpson'))
   rchao=c(rchao,chao1(table(u)))
 }
-
-
 rare_rich=data.frame("deep"=seq(1,length(rare_curve), by=100),"Richesse"=rc)
 rare_chao=data.frame("deep"=seq(1,length(rare_curve), by=100),"Richesse"=rchao)
 
@@ -35,17 +34,20 @@ plot(rare_rich, ylim=c(range(rare_rich$Richesse, rare_chao$Richesse)))
 points(rare_chao, col='red')
 sd(tail(rc, 10))
 
-######déstructuration de la table de comptage pour voir la différence des reads.
-new_data=as.numeric(sort(matrix_otu[1,which(matrix_otu[1,]>0)], decreasing = T))
-mean(new_data)
-plot(new_data)
-
+##Choix au hazard de 200 bactéries 100x pour étudier la variabilité des indices de diversité
+set.seed(123)
 cnt=1
-while (cnt<300){
-  inf=new_data<mean(new_data)
-  new_data[inf==T]=as.numeric(new_data[inf==T])*2
-  new_data[inf==F]=as.numeric(new_data[inf==F])/2
-  points(new_data)
+rsimpson=rshannon=rchao=rc=NULL
+while (cnt<61){
+  u=sample(as.factor(rare_curve), size=200)
+  rc=c(rc,length(unique(u)))
+  rsimpson=c(rsimpson,diversity(table(u), index='simpson'))
+  rshannon=c(rshannon,diversity(table(u), index='shannon'))
+  rchao=c(rchao,chao1(table(u)))
   cnt=cnt+1
 }
+boxplot(rc)
+boxplot(rsimpson)
+boxplot(rshannon)
+boxplot(rchao)
 
